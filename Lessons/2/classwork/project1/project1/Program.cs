707cc324b89c39e72998e7c1e0ccabb203e33787
ErvinLubenovich <luervin@yandex.ru>
2020-03-16 22:10:36 +0300

    ErvinLubenovich

---
+++ Lessons/2/classwork/project1/project1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace project1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = 10;
+            Decimal number2 = 20; 
+        }
+    }
+}
